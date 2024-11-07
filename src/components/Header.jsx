f995861b3d77972005f9ec9d545d42742840a8b8
Joe Dixon <joedixon21@gmail.com>
2024-11-07 12:43:08 +0000

    Made topics header more dynamic with map

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,20 @@
 import { Link } from "react-router-dom";
 
 export default function Header() {
+  const allTopics = ["cooking", "coding", "football"];
+
   return (
     <header className="header">
       <h1>NC News</h1>
       <nav>
         <Link to="/">Home</Link>
-        <Link to="/topics/cooking">Cooking</Link>
-        <Link to="/topics/coding">Coding</Link>
-        <Link to="/topics/football">Football</Link>
+        {allTopics.map((topic) => {
+          return (
+            <Link key={topic} to={`/topics/${topic}`}>
+              {topic.charAt(0).toUpperCase() + topic.slice(1)}
+            </Link>
+          );
+        })}
       </nav>
     </header>
   );
